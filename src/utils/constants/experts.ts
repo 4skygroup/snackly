0b476182a121acf1ec4621b45d55d29e7c90856f
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-05-27 15:30:16 +0200

    Offer Page Cards implementation

---
+++ src/utils/constants/experts.ts
@@ -0,0 +1,32 @@
+export const experts = [
+    {
+        name: "Maxime Gallery",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        link: "https://linkedin.com"
+    },
+    {
+        name: "Maxime Gallery",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        link: "https://linkedin.com"
+    },
+    {
+        name: "Maxime Gallery",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        link: "https://linkedin.com"
+    },
+    {
+        name: "Maxime Gallery",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        link: "https://linkedin.com"
+    },
+    {
+        name: "Maxime Gallery",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        link: "https://linkedin.com"
+    },
+    {
+        name: "Maxime Gallery",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        link: "https://linkedin.com"
+    },
+]
---
+++ src/utils/constants/offers.ts
@@ -0,0 +1,14 @@
+export const offers = {
+    start: {
+        name: "Start",
+        offer: 5
+    },
+    gold: {
+        name: "Gold",
+        offer: 10
+    },
+    premium: {
+        name: "Premium",
+        offer: 20
+    }
+}

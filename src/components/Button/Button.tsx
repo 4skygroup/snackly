9af1e7bf5b8b8de7e80ff3772b5936c85ad18e37
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-05-27 15:26:49 +0200

    Call to Action

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button() {
+  return (
+    <button className="text-t5 text-snackly-blue font-bold bg-white px-7.5 py-3.75 rounded-2xl">
+      Contactez-nous
+    </button>
+  );
+}

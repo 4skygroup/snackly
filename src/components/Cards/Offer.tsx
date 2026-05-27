c571dd1a3a7beaf86d30d9d868397e4a6e8fab80
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-05-27 15:28:53 +0200

    Offer Page Cards implementation

---
+++ src/components/Cards/Offer.tsx
@@ -0,0 +1,36 @@
+import { offers } from "../../utils/constants/offers";
+import Button from "../Button/Button";
+
+export default function Offer({
+  isPopular = false,
+  imgSrc = "/images/Snack-Content 1.jpg",
+  offer = "start",
+}: {
+  isPopular?: boolean;
+  imgSrc?: string;
+  offer?: "start" | "gold" | "premium";
+}) {
+  return (
+    <article
+      className={`w-65.75 h-73.75 ${isPopular ? "bg-snackly-purple/50" : "bg-snackly-card-gray/18"} rounded-r1 flex flex-col`}
+    >
+      <div className="w-full h-53.75 flex justify-center items-center bg-snackly-card-gray/44 rounded-r1">
+        <div
+          className={`w-61.25 h-fit gap-y-0.75 bg-[url(${imgSrc})] rounded-r1 flex flex-col items-center object-cover object-center`}
+        >
+          <span className="font-bold text-t3">{offers[offer].name}</span>
+          <span className="font-bold text-h5">{offers[offer].offer}+</span>
+          <span className="font-bold text-t3">videos</span>
+          <Button />
+        </div>
+      </div>
+      {isPopular ? (
+        <span className="m-auto text-t5 px-5 py-1.5 bg-snackly-purple rounded-r3">
+          le plus populaire
+        </span>
+      ) : (
+        <></>
+      )}
+    </article>
+  );
+}
---
+++ src/types/ExpertProp.ts
@@ -0,0 +1,5 @@
+export interface ExpertProp {
+    name: string,
+    description: string,
+    link: string
+}

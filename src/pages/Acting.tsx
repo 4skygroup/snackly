4d891109ddfba5a20ed8952a9fb5813dfe917d59
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-05-29 11:03:27 +0200

    Pages structure and basic styles

--- src/pages/Acting.tsx
+++ src/pages/Acting.tsx
@@ -1,72 +1,92 @@
 import Offer from "../components/Cards/Offer";
 import { experts } from "../utils/constants/experts";
 import Expert from "../components/Cards/Expert";
+import Footer from "../components/Footer/Footer";
+import { policy } from "../utils/constants/policy";
+import { services } from "../utils/constants/services";
+
 export default function Acting() {
   return (
     <div>
-      <section>
-        <div>
-          <p>Démarrez votre projet</p>
-          <span>Acting</span>
-        </div>
-        <div>
-          <span>Acting</span>
-          <span>Déclinaison</span>
-          <span>Brief</span>
-          <span>Script</span>
-          <span>Montage</span>
-          <span>Tournage</span>
-        </div>
-      </section>
-      <section>
-        <div>
-          {/* Left */}
+      <section className="bg-linear-to-b from-snackly-purple to-snackly-blue text-white flex flex-col gap-y-25 py-15 px-2.5 items-center">
+        <section className="flex justify-between">
+          <div className="flex flex-col">
+            <p className="text-h5 uppercase">Démarrez votre projet</p>
+            <span className="text-h2 font-bold uppercase">Acting</span>
+          </div>
           <div>
-            {/* Top */}
-            <div>
-              <p>Des tournages de qualité</p>
-              <p>Un snack content personnalisé</p>
+            <span className="text-t3 uppercase w-36.25 h-36.25 bg-white/90 border-5 text-snackly-purple flex items-center justify-center rounded-full border-white border-solid">
+              Acting
+            </span>
+            <span className="bg-white/20 text-t3 px-3.75 py-2 text-white">
+              Déclinaison
+            </span>
+            <span className="bg-white/20 text-t3 px-3.75 py-2 text-white">
+              Brief
+            </span>
+            <span className="bg-white/20 text-t3 px-3.75 py-2 text-white">
+              Script
+            </span>
+            <span className="bg-white/20 text-t3 px-3.75 py-2 text-white">
+              Montage
+            </span>
+            <span className="bg-white/20 text-t3 px-3.75 py-2 text-white">
+              Tournage
+            </span>
+          </div>
+        </section>
+        <section className="max-w-300 bg-white/18 mx-auto p-20">
+          <div className="flex gap-x-25">
+            {/* Left */}
+            <div className="flex flex-col gap-y-7.5">
+              {/* Top */}
+              <div className="flex flex-col gap-y-1.5">
+                <p className="text-t1">Des tournages de qualité</p>
+                <p className="text-t5">Un snack content personnalisé</p>
+              </div>
+              {/* Bottom */}
+              <div className="flex flex-col gap-y-6">
+                <div className="flex gap-x-10">
+                  <img src="/icons/Script.svg" alt="" width="30px" />
+                  <p className="text-t3">Accroches et messages percutants</p>
+                </div>
+                <div className="flex gap-x-10">
+                  <img src="/icons/Subtitle.svg" alt="" width="30px" />
+                  <p className="text-t3">Sous-titrage et audio-description</p>
+                </div>
+                <div className="flex gap-x-10">
+                  <img src="/icons/Ratio.svg" alt="" width="30px" />
+                  <p className="text-t3">Formats adaptés</p>
+                </div>
+                <div className="flex gap-x-10">
+                  <img src="/icons/Clap.svg" alt="" width="30px" />
+                  <p className="text-t3">Réalisation et montages impactants</p>
+                </div>
+                <div className="flex gap-x-10">
+                  <img src="/icons/Design.svg" alt="" width="30px" />
+                  <p className="text-t3">Direction artistique fidèle</p>
+                </div>
+              </div>
             </div>
-            {/* Bottom */}
-            <div>
-              <div>
-                <img src="" alt="" />
-                <p>Accroches et messages percutants</p>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <p>Sous-titrage et audio-description</p>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <p>Formats adaptés</p>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <p>Réalisation et montages impactants</p>
-              </div>
-              <div>
-                <img src="" alt="" />
-                <p>Direction artistique fidèle</p>
-              </div>
+            {/* Right */}
+            <div className="w-125 h-98.75 bg-linear-to-b from-snackly-purple to-snackly-dark-purple">
+              {/* Animation */}
             </div>
           </div>
-          {/* Right */}
-          <div>{/* Animation */}</div>
-        </div>
+        </section>
+        <section className="flex gap-x-50">
+          <Offer imgSrc="/images/Snack-Content-1.jpg" />
+          <Offer
+            isPopular={true}
+            imgSrc="/images/Snack-Content-2.jpg"
+            offer="gold"
+          />
+          <Offer imgSrc="/images/Snack-Content-3.jpg" offer="premium" />
+        </section>
       </section>
-      <section>
-        <Offer />
-        <Offer
-          isPopular={true}
-          imgSrc="/images/Snack-Content 2.jpg"
-          offer="gold"
-        />
-        <Offer imgSrc="/images/Snack-Content 3.jpg" offer="premium" />
-      </section>
-      <section>
-        <p>Nos experts</p>
-        <div>
+      <section className="flex flex-col p-25 gap-y-17.5 items-center">
+        <p className="text-h4 text-snackly-blue">Nos experts</p>
+        <div className="flex overflow-x-scroll gap-12.5 max-w-310 py-1.25 no-scrollbar">
           {experts.map((expert, index) => (
             <Expert
               key={index}
@@ -77,6 +97,7 @@ export default function Acting() {
           ))}
         </div>
       </section>
+      <Footer policy={policy} services={services} />
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,119 @@
+// import Button from "../components/Button/Button";
+// import Expert from "../components/Cards/Expert";
+// import Offer from "../components/Cards/Offer";
+import { Link } from "react-router";
 import Button from "../components/Button/Button";
-import Expert from "../components/Cards/Expert";
-import Offer from "../components/Cards/Offer";
 import Footer from "../components/Footer/Footer";
-import { experts } from "../utils/constants/experts";
 import { policy } from "../utils/constants/policy";
 import { services } from "../utils/constants/services";
-
+import Tag from "../components/Tag/Tag";
+import Partner from "../components/Partner/Partner";
+import Meta from "/images/Meta.png";
+import Tiktok from "/images/Tiktok.png";
+import SnapChat from "/images/SnapChat.png";
+import Youtube from "/images/Youtube.png";
+import Google from "/images/Google.png";
 export default function Home() {
   return (
     <div className="text-white bg-gray-white">
-      <Button />
-      {experts.map((expert, index) => (
-        <Expert
-          key={index}
-          name={expert.name}
-          description={expert.description}
-          link={expert.link}
-        />
-      ))}
-      <Offer />
-      <Offer
-        isPopular={true}
-        imgSrc="/images/Snack-Content 2.jpg"
-        offer="gold"
-      />
-      <Offer imgSrc="/images/Snack-Content 3.jpg" offer="premium" />
+      <section>
+        <div className="bg-linear-to-b from-snackly-purple to-snackly-blue flex justify-center items-center gap-x-20 py-25">
+          {/* Left */}
+          <div className="max-w-183.75 ">
+            <p className="uppercase text-h2 flex flex-col leading-28.75 pb-2.5">
+              Snack Content Agency
+            </p>
+            <p className="text-t1 max-w-177 leading-9.5 pb-7.5">
+              Des créations vidéo pensées pour performer sur tous vos leviers
+              digitaux.
+            </p>
+            <Button />
+          </div>
+          {/* Right */}
+          <div>
+            <img src="/images/Snackly Video.png" alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="bg-black flex flex-col items-center py-17.5 px-5 gap-y-10">
+        <p className="text-t1">Le Snack Content à votre portée</p>
+        <div className="flex flex-col gap-7.5">
+          {/* Top */}
+          <nav className="flex justify-between w-full">
+            <Tag content="Créativité dans l’exécution" />
+            <Tag content="Contenu percutant" isOpaque={true} />
+            <Tag content="Idéal pour l’espace pub" isOpaque={true} />
+          </nav>
+          {/* Bottom */}
+          <div className="max-w-278.75 px-2.5">
+            <p className="text-t3">
+              Nos créations sont pensées pour générer de la performance sur
+              l’ensemble du funnel média, de la notoriété à la conversion.
+              <br />
+              Nos experts repèrent les meilleures idées du web et les
+              transforment en vidéos impactantes, alignées avec les stratégies
+              médias de nos clients.
+            </p>
+            {/* <p className="text-t3">
+              Chaque jour, un internaute est exposé à plus de 15 000
+              sollicitations commerciales. Pour éviter la lassitude liée à la
+              répétition de contenus similaires, les algorithmes de diffusion
+              privilégient des contenus renouvelés et régulièrement mis à jour.
+            </p>
+            <p className="text-t3">
+              Chaque canal de diffusion répond à ses propres codes : formats
+              publicitaires, durées, usages des audiences. Une création
+              performante est avant tout une création pensée pour le média qui
+              la porte.
+            </p> */}
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col gap-y-25 py-50 bg-linear-to-b from-snackly-blue to-snackly-purple">
+        <div className="bg-white w-162.5 rounded-tr-full rounded-br-full flex flex-col justify-center items-end gap-y-5 h-125 px-17.5">
+          <span className="text-h1 text-snackly-purple font-bold ">ACTING</span>
+          <span className="text-t4 text-snackly-purple/50 font-bold">
+            Survolez-moi
+          </span>
+          {/* <p>
+            Des contenus stratégiques pensés pour maximiser votre impact média
+          </p>
+          <p>
+            Nous concevons et produisons des contenus personnalisés, alignés sur
+            vos objectifs de visibilité et de performance. Nos équipes
+            organisent des tournages adaptés à votre projet
+          </p> */}
+          <Link to="/action">En savoir plus</Link>
+        </div>
+        <div className="bg-white w-162.5 rounded-tl-full rounded-bl-full flex flex-col justify-center items-start gap-y-5 h-125 px-17.5 self-end">
+          <span className="text-h1 text-snackly-blue font-bold">MOTION</span>
+          <span className="text-t4 text-snackly-blue/50 font-bold">
+            Survolez-moi
+          </span>
+          {/* <p>
+            Des contenus stratégiques pensés pour maximiser votre impact média
+          </p>
+          <p>
+            Nous concevons et produisons des contenus personnalisés, alignés sur
+            vos objectifs de visibilité et de performance. Nos équipes
+            organisent des tournages adaptés à votre projet
+          </p> */}
+          <Link to="/motion">En savoir plus</Link>
+        </div>
+      </section>
+      {/* Icones des partenaires */}
+      <section className="bg-black flex items-end gap-x-12.5 px-10 py-15 justify-center">
+        <Partner src={Meta} alt="Meta" label="Business Partner" />
+        <Partner src={Tiktok} alt="Tiktok" label="Marketing Partner" />
+        <Partner src={Google} alt="Google" label="Google Partner" />
+        <Partner src={Youtube} alt="Youtube" label="Youtube" />
+        <Partner src={SnapChat} alt="SnapChat" label="SnapChat" />
+      </section>
+      <section>
+        <span>Foire aux questions</span>
+        {/* Foire aux questions */}
+        <div></div>
+      </section>
       <Footer policy={policy} services={services} />
     </div>
   );

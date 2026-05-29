cfa32b2b547874295a1fbdc7f8126502c06c6855
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-05-29 11:55:48 +0200

    Integration without animations

--- src/components/Cards/Offer.tsx
+++ src/components/Cards/Offer.tsx
@@ -16,7 +16,12 @@ export default function Offer({
     >
       <div className="w-full h-53.75 flex justify-center items-center bg-snackly-card-gray/44 rounded-r1">
         <div
-          className={`w-61.25 h-fit gap-y-0.75 bg-[url(${imgSrc})] rounded-r1 flex flex-col items-center object-cover object-center`}
+          style={{
+            backgroundImage: `linear-gradient(rgba(58, 12, 163, 0.6), rgba(58, 12, 163, 0.6)), url(${imgSrc})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+          className={`w-61.25 h-fit gap-y-0 bg-snackly-blue/30 rounded-r1 flex flex-col items-center object-cover object-center`}
         >
           <span className="font-bold text-t3">{offers[offer].name}</span>
           <span className="font-bold text-h5">{offers[offer].offer}+</span>
